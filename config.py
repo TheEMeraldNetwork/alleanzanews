8bf61c9477e040e5c7b9bc6dc8e02dd4fb024136
TheEMeraldNetwork <davide.consiglio@icloud.com>
2025-02-21 15:24:24 +0100

    Added topic lists section after Venn diagram and created backup 2025-02-21_15-23

---
+++ config.py
@@ -0,0 +1,30 @@
+COMPANY_NAMES = [
+    "Alleanza Assicurazioni",
+    "Unidea Assicurazioni",
+    "Vita Nuova"
+]
+
+COMPANY_VARIATIONS = {
+    "Alleanza Assicurazioni": ["Alleanza", "Alleanza Assicurazioni", "Alleanza Assicurazioni S.p.A."],
+    "Unidea Assicurazioni": ["Unidea", "Unidea Assicurazioni", "Unidea Assicurazioni S.p.A."],
+    "Vita Nuova": ["Vita Nuova", "Vita Nuova Assicurazioni", "Vita Nuova S.p.A."]
+}
+
+NEWS_SEARCH = {
+    "start_date": "2024-01-01",
+    "end_date": "2025-01-31",
+    "language": "it",
+    "region": "IT",
+    "articles_per_company": 10
+}
+
+INSURANCE_TERMS = [
+    "assicurazioni",
+    "assicurazione",
+    "polizza",
+    "broker",
+    "finanziaria",
+    "previdenza",
+    "risparmio",
+    "investimento"
+] 
